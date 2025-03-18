22f5e14f256ff535d8045c65a0970695809b4d0a
KellyAndreaCalle <kellycalle317298@correo.itm.edu.co>
2025-03-17 23:09:04 -0500

    calcular edad

--- HelloWork/Program.cs
+++ HelloWork/Program.cs
@@ -2,9 +2,35 @@
 
     class Program{
         static void  Main(){
-            const string Name="Kelly Calle";
-            Console.WriteLine($"Hello World! {Name}");
+
+            DateOnly dateConverted= new DateOnly();
+            string nameInput;
+            string birthdayInput; 
+            Console.WriteLine("Hola, bienvenid@ al calculador de edad!");
+            Console.WriteLine("Escribe tu nombre: ");
+             nameInput = Console.ReadLine();
+            Console.WriteLine($"Un gusto en conocerte {nameInput} ");
+            Console.WriteLine("Escribe tu fecha de nacimiento en formato dd/mm/yy: ");
+             birthdayInput = Console.ReadLine();
+             bool isDataValid=DateOnly.TryParse(birthdayInput,out dateConverted);
+             if(isDataValid==false) Console.WriteLine($"La fecha de nacimiento es invalida, usted nos envio este dato erroneo {birthdayInput}");
+             var person= new Person{
+                name=nameInput,
+                Birthday=dateConverted,
+                Age = DateTime.Now.Year - dateConverted.Year
+
+             };
+            Console.WriteLine($"Tu nombre: {person.name}");
+            Console.WriteLine($"Tu fecha de nacimiento: {person.Birthday}");
+            Console.WriteLine($"Tu edad es: {person.Age} años");
+
             Console.WriteLine();
         }
+    }
+  
+    public class Person{
+        public string name {get; set;}
+        public DateOnly Birthday {get; set;}
+        public int Age {get; set;}
     }
 }
